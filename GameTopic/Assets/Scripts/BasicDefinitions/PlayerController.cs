1d12872b877aa2abf1b76e884be6b3de2b4db2d7
Crazyfighting <83546004+Crazyfighting@users.noreply.github.com>
2023-05-23 17:42:16 +0800

    Update PlayerController.cs

--- GameTopic/Assets/Scripts/BasicDefinitions/PlayerController.cs
+++ GameTopic/Assets/Scripts/BasicDefinitions/PlayerController.cs
@@ -3,3 +3,11 @@ public interface IPlayerController{
     void CreateDeviceInstance(IDeviceInfo info);
     
 }
+public class PlayerController : IPlayerController
+{
+    public void CreateDeviceInstance(IDeviceInfo info)
+    {
+        string encodedInfo = info.Encode();
+        info.Decode(encodedInfo);
+    }
+}
