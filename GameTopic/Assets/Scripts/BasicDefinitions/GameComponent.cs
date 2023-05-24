14472c4d97b4002424def81eced718c6d59c1dbc
Chengpong1127 <Chengpong1127@gmail.com>
2023-05-24 19:08:41 +0800

    create ConnecterPoint

--- GameTopic/Assets/Scripts/BasicDefinitions/GameComponent.cs
+++ GameTopic/Assets/Scripts/BasicDefinitions/GameComponent.cs
@@ -1,12 +1,14 @@
+using System.Collections.Generic;
+
 // Description: Defines the basic interfaces for the game components.
 public interface IGameComponent
 {
-
+    Dictionary<ConnecterPoint, ConnecterPoint> ConnecterMap { get; }
 }
 
 public interface IConnector
 {
-
+    
 }
 
 public interface ICoreComponent
