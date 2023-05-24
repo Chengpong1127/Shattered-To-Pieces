e54d0622ca66c7b7f9cbb7f591bd4a68a5a42408
Chengpong1127 <Chengpong1127@gmail.com>
2023-05-24 18:49:12 +0800

    Define basic component

---
+++ GameTopic/Assets/Scripts/GameComponent/GameComponent.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameComponent : MonoBehaviour, IGameComponent
+{
+    public int GameComponentID;
+    private IConnector connector;
+    private ICoreComponent coreComponent;
+
+    public IConnector Connector
+    {
+        get
+        {
+            return connector;
+        }
+    }
+
+    public ICoreComponent CoreComponent
+    {
+        get
+        {
+            return coreComponent;
+        }
+    }
+
+    public void DumpInfo(){
+
+    }
+}
