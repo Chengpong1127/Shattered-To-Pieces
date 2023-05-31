1c5fadc63625797f3cddbfa4878bbe3c1823f452
Chengpong1127 <Chengpong1127@gmail.com>
2023-05-31 17:39:01 +0800

    Complete Scene2 Function

--- GameTopic/Assets/Scripts/BasicDefinitions/Info.cs
+++ GameTopic/Assets/Scripts/BasicDefinitions/Info.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using UnityEngine;
 public class DeviceInfo
 {
 
@@ -14,6 +15,18 @@ public class DeviceInfo
     {
         var result = JsonConvert.SerializeObject(this);
         return result;
+    }
+    public void printAllInfo(){
+        if (GameComponentInfoMap == null){
+            Debug.Log("GameComponentInfoMap is null");
+            return;
+        }
+        if (GameComponentInfoMap.Count == 0){
+            Debug.Log("GameComponentInfoMap is empty");
+        }
+        //print component number
+        Debug.Log("GameComponentInfoMap.Count: " + GameComponentInfoMap.Count);
+
     }
 }
 
--- GameTopic/Assets/Scripts/Temp/TempSaver.cs
+++ GameTopic/Assets/Scripts/Temp/TempSaver.cs
@@ -4,12 +4,54 @@ using UnityEngine;
 
 public class TempSaver : MonoBehaviour
 {
+    private Device DeviceObject;
+    private DeviceInfo DefaultInfo(){
+        var info = new DeviceInfo();
+        info.GameComponentInfoMap.Add(0, new GameComponentInfo{
+            componentGUID = 0,
+            connectorInfo = new ConnectorInfo{
+                linkedConnectorID = -1,
+                linkedTargetID = -1,
+            }
+        });
+        return info;
+    }
+    private void Start() {
+        for(int i = 0; i < 3; i++){
+            infos[i] = DefaultInfo();
+        }
+        load(0);
+    }
     public void Create0(){
         var component = GameComponentFactory.Instance.CreateComponent(0);
-        component.transform.position = new Vector3(0, 10, 0);
+        component.transform.position = new Vector3(0, 5, 0);
     }
     public void Create1(){
         var component = GameComponentFactory.Instance.CreateComponent(1);
-        component.transform.position = new Vector3(0, 10, 0);
+        component.transform.position = new Vector3(0, 5, 0);
+    }
+    private int currentDeviceID = 0;
+    private DeviceInfo[] infos = new DeviceInfo[3];
+    public void ChooseInfo(int id){
+        currentDeviceID = id;
+        load(id);
+    }
+    public void Save(){
+        Debug.Log("Save");
+        infos[currentDeviceID] = DeviceObject.DumpDevice();
+        infos[currentDeviceID].printAllInfo();
+    }
+    public void load(int id){
+        CleanAllGameComponent();
+        var info = infos[id];
+        var gameobj = DeviceFactory.Instance.CreateDevice(info);
+        DeviceObject = gameobj.GetComponent<Device>();
+        gameobj.transform.position = new Vector3(0, 5, 0);
+    }
+    private void CleanAllGameComponent(){
+        var components = GameObject.FindObjectsOfType<GameComponent>();
+        foreach(var component in components){
+            Destroy(component.gameObject);
+        }
     }
 }
