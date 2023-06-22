10e5060132d610351ce68a087385eacdbc4dc458
fili-asphalt <styuiw9468546@gmail.com>
2023-06-22 14:20:10 +0800

    define state in rb connector

--- GameTopic/Assets/Scripts/Connector/RBconnector.cs
+++ GameTopic/Assets/Scripts/Connector/RBconnector.cs
@@ -1,7 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Events;
 
 public enum ConnectorState
 {
@@ -15,6 +16,7 @@ public class RBconnector : MonoBehaviour, IConnector
 {
     public int connectorID { get; set; }
     public ConnectorState currState { get; set; } = ConnectorState.INITIAL;
+    UnityEvent<bool> attachHandler = new UnityEvent<bool>();
 
     [SerializeField] Rigidbody2D selfRigidbody;
     [SerializeField] Collider2D selfCollider;
@@ -30,12 +32,40 @@ public class RBconnector : MonoBehaviour, IConnector
         
     }
 
+    void SwitchInitial(bool b) { }
+    void SwitchCombine(bool b) {
+        if(!b) { SwitchSelecting(false); }
+        currState = ConnectorState.COMBINE;
+        selfRigidbody.freezeRotation = b;
+        SwitchTargetActive(b);
 
+        if(!b) { currState = ConnectorState.INITIAL; }
+    }
+    void SwitchSelecting(bool b) {
+        if(currState != ConnectorState.SELECT && currState != ConnectorState.COMBINE) { return; }
+        currState = ConnectorState.SELECT;
 
+        selfRigidbody.gravityScale = b ? 0 : 1;
+        selfCollider.isTrigger = b;
+        SwitchTargetActive(!b);
+        attachHandler.Invoke(b);
+    }
+    void SwitchAttach(bool b) {
+        if (currState != ConnectorState.ATTACH && currState != ConnectorState.COMBINE) { return; }
+        currState = ConnectorState.ATTACH;
 
+        SwitchTargetActive(!b);
+        selfCollider.isTrigger = b;
+        selfRigidbody.gravityScale = b ? 0 : 1;
+        attachHandler.Invoke(b);
+    }
+    void SwitchTargetActive(bool b) {
+        targetList.ForEach(target => {
+            target.SwitchActive(b);
+        });
+    }
 
-
-
+    // implement interface
 
     public GameObject GetTargetObjByIndex(int targetID) { return gameObject; }
     public void ConnectToComponent(IConnector connectorPoint, ConnectorInfo info) { }
