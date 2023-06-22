4cb8ce6fecf5a9b1c84475400b23b0bcd90fa215
fili-asphalt <styuiw9468546@gmail.com>
2023-06-22 23:14:18 +0800

    shiftcRBconnector to Connector

--- GameTopic/Assets/Scripts/Connector/Connector.cs
+++ GameTopic/Assets/Scripts/Connector/Connector.cs
@@ -6,257 +6,190 @@ using UnityEngine.Events;
 using static UnityEditor.PlayerSettings;
 using static UnityEngine.GraphicsBuffer;
 
-/*
- * �Ȯɴ��դ�k : 
- * �� Z �i�J�s��Ҧ��AX �h�X�s��Ҧ�
- * �b�s��Ҧ����ϥηƹ��ӿ������ë����ƹ��즲�ӱN����s�����L����W
- * 
- * �b unity �s�边���ݭn���N rigidbody ��unity ����s���ܥ��}���_�h�|����
- * 
- */
+
 public class Connector : MonoBehaviour, IConnector
 {
-    public int connectorID { get; set; }    // self unique id
+    public int connectorID { get; set; }
+    public ConnectorState currState { get; set; } = ConnectorState.INITIAL;
+    UnityEvent<bool> attachHandler = new UnityEvent<bool>();
+    Vector2 movePosition;
 
-    bool combineMode;   // is the game in combine mode
-    bool selecting;     // is this connector be selecting
-
-
-    // Some variable Connector should have and should be assign in unity editor.
     [SerializeField] Rigidbody2D selfRigidbody;
     [SerializeField] Collider2D selfCollider;
     [SerializeField] AnchoredJoint2D selfJoint;
     [SerializeField] List<Target> targetList;
 
-    // for record connector linking state
-    public UnityEvent<bool> linkedHandler;
-    GameObject selectedTargetObj;
-    Target linkedTarget;
+    Target linkedTarget = null;
+    Target detectedTarget = null;
 
-    // some variable for detect hited trigget when the connector itself be selecting
+    // target detect process
     static ContactFilter2D targetLayerFilter = new ContactFilter2D();
+    List<Collider2D> collisionResult = new List<Collider2D>();
     float selectedObjDist;
     float compareObjDist;
-    List<Collider2D> collisionResult;
-    Vector2 movePosition;
 
-    public event Action<IConnector> OnConnectConnector;
-    public event Action<int> OnDisconnectConnector;
-    public event Action<IConnector> OnConnectorIsConnected;
-    public event Action<int> OnConnectorIsDisconnected;
+    //==================================functions==================================//
 
-    private void Awake()
-    {
-        if(selfRigidbody == null || selfCollider == null || selfJoint == null) { Debug.Log("Missing variable in Connector.\n"); }
+
+    private void Awake() {
         Debug.Assert(selfRigidbody);
         Debug.Assert(selfCollider);
         Debug.Assert(selfJoint);
+
+        linkedTarget = null;
+        selectedObjDist = float.PositiveInfinity;
 
         // initialize for filter
         targetLayerFilter.useLayerMask = true;
         targetLayerFilter.useTriggers = true;
         targetLayerFilter.SetLayerMask(LayerMask.GetMask("targetLayer"));// this string should be Target's layer
 
-
-        int loopindex = 0;
+        int tid = 0;
         targetList.ForEach(target => {
-            target.SetOwner(this);
-            target.targetID = loopindex++;
+            target.ownerConnector = this;
+            target.targetID = tid++;
         });
-        selectedObjDist = float.PositiveInfinity;
-        selectedTargetObj = null;
-        linkedTarget = null;
-        collisionResult = new List<Collider2D>();
-        linkedHandler = new UnityEvent<bool>();
     }
 
-    private void Update()
-    {
-        if (Input.GetKey(KeyCode.Z))
-        {
-            SwitchCombineMode(true);
+
+    private void Update() {
+        if (Input.GetKey(KeyCode.Z)) {
+            SwitchCombine(true);
         }
-        if (Input.GetKey(KeyCode.X))
-        {
-            SwitchCombineMode(false);
+        if (Input.GetKey(KeyCode.X)) {
+            SwitchCombine(false);
         }
     }
 
-    private void OnMouseDown()
-    {
+    private void OnMouseDown() {
         SwitchSelecting(true);
     }
-    private void OnMouseUp()
-    {
+    private void OnMouseUp() {
         SwitchSelecting(false);
+
+        (IConnector ic, int tid) = GetAvailableConnector();
+        if (ic == null || tid == -1) { return; }
+        ConnectorInfo tinfo = new ConnectorInfo();
+        tinfo.connectorID = this.connectorID;
+        tinfo.linkedConnectorID = ic.connectorID;
+        tinfo.linkedTargetID = tid;
+        tinfo.connectorRotation = 0f;
+
+        ConnectToComponent(ic, tinfo);
     }
-    private void OnMouseDrag()
-    {
-        if (!combineMode || !selecting) { return; }
-        HitTriggerUpdate();
+
+    private void OnMouseDrag() {
+        if (!(currState == ConnectorState.SELECT)) { return; }
+        DetectTarget();
         TrackPositionUpdate((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition));
     }
 
-    // dump connecotr info with ConnectorInfo structure
-    public ConnectorInfo Dump()
-    {
-        ConnectorInfo info = new ConnectorInfo();
-        info.connectorID = connectorID;
-        info.linkedConnectorID = linkedTarget != null ? linkedTarget.ownerConnector.connectorID : -1;
-        info.linkedTargetID = linkedTarget != null ? linkedTarget.targetID : -1;
+    // State chang function
+    void SwitchCombine(bool b) {
+        if (!b) { SwitchSelecting(false); }
+        currState = ConnectorState.COMBINE;
+        selfRigidbody.freezeRotation = b;
+        SwitchTargetActive(b);
 
-        return info;
+        if (!b) { currState = ConnectorState.INITIAL; }
     }
+    void SwitchSelecting(bool b) {
+        if (currState != ConnectorState.SELECT && currState != ConnectorState.COMBINE) { return; }
+        currState = ConnectorState.SELECT;
 
-    public void LoadID(int Cid)
-    {
-        connectorID = Cid;
+        if (b) { this.UnlinkToConnector(); }
+
+        selfRigidbody.gravityScale = b ? 0 : 1;
+        selfCollider.isTrigger = b;
+        SwitchTargetActive(!b);
+        attachHandler.Invoke(b);
+
+        if (!b) { currState = ConnectorState.COMBINE; }
     }
+    void SwitchAttach(bool b) {
+        if (currState != ConnectorState.ATTACH && currState != ConnectorState.COMBINE) { return; }
+        currState = ConnectorState.ATTACH;
 
-    // move connector to  position by rigidbody.
-    void TrackPositionUpdate(Vector2 pos)
-    {
-        selfRigidbody.MovePosition(pos);
+        SwitchTargetActive(!b);
+        selfCollider.isTrigger = b;
+        selfRigidbody.gravityScale = b ? 0 : 1;
+        attachHandler.Invoke(b);
+
+        if (!b) { currState = ConnectorState.COMBINE; }
     }
-
-    // show target in screen or not.
-    void SwitchTargetActive(bool b)
-    {
+    void SwitchTargetActive(bool b) {
         targetList.ForEach(target => {
             target.SwitchActive(b);
         });
     }
 
-    // update collider list to check the connector hit which target.
-    // update every frame when connector be select.
-    void HitTriggerUpdate()
-    {
+    // Target interact
+    void LinkToConnector(Connector connector, ConnectorInfo info) {
+        if (connector == null ||
+            connector.connectorID != info.connectorID ||
+            this.connectorID != info.linkedConnectorID ||
+            !(connector.targetList.Count > info.linkedTargetID) ||
+            !connector.targetList[info.linkedTargetID].LinkToTarget(this)
+            ) { return; }
+
+        this.transform.rotation = Quaternion.Euler(0, 0, info.connectorRotation);
+
+        linkedTarget = connector.targetList[info.linkedTargetID];
+
+        linkedTarget.ownerConnector.attachHandler.AddListener(this.SwitchAttach);
+        this.selfJoint.connectedBody = connector.selfRigidbody;
+        this.selfJoint.connectedAnchor = (Vector2)linkedTarget.targetPoint.transform.localPosition;
+        this.selfJoint.enabled = true;
+    }
+    void UnlinkToConnector() {
+        this.selfJoint.enabled = false;
+
+        if (linkedTarget == null) { return; }
+
+        linkedTarget.UnLinkToTarget();
+        linkedTarget.ownerConnector.attachHandler.RemoveListener(this.SwitchAttach);
+        linkedTarget = null;
+    }
+    public void DetectTarget() {
+        GameObject selectedTargetObj = null;
+        selectedObjDist = float.PositiveInfinity;
         selfCollider.OverlapCollider(targetLayerFilter, collisionResult);
-        if(collisionResult.Count == 0) {
-            selectedTargetObj = null;
-            selectedObjDist = float.PositiveInfinity;
-            return;
-        }
-        collisionResult.ForEach(c =>
-        {
+
+        collisionResult.ForEach(c => {
             compareObjDist = selfCollider.Distance(c).distance;
-            if(compareObjDist < selectedObjDist)
-            {
+            if (compareObjDist < selectedObjDist) {
                 selectedTargetObj = c.gameObject;
             }
         });
+
+        detectedTarget = selectedTargetObj?.GetComponent<Target>();
     }
 
-    // call this function when the connector link to other connector is selected ot attach that selected connector.
-    // process by connector themself.
-    public void SwitchLinkingSelect(bool b)
-    {
-        SwitchTargetActive(!b);
-        selfCollider.isTrigger = b;
-        selfRigidbody.gravityScale = b ? 0 : 1;
-        linkedHandler.Invoke(b);
+    void TrackPositionUpdate(Vector2 pos) {
+        selfRigidbody.MovePosition(pos);
     }
 
-    // reset when the selecting start.
-    void ResetCompareVariable()
-    {
-        selectedTargetObj = null;
-        selectedObjDist = float.PositiveInfinity;
-        linkedTarget = null;
+    // implement interface
+
+    public GameObject GetTargetObjByIndex(int targetID) {
+        return this.targetList.Count > targetID ? this.targetList[targetID].gameObject : null;
     }
-
-    // break the link between two connector.
-    void BreakTatgetLink() {
-        selfJoint.enabled = false;
-
-        if(linkedTarget == null) { return; }
-
-        linkedTarget.UnLinkToTarget();
-        linkedTarget.ownerConnector.linkedHandler.RemoveListener(this.SwitchLinkingSelect);
-
-        OnDisconnectConnector?.Invoke(linkedTarget.ownerConnector.connectorID);
-        linkedTarget.ownerConnector.OnConnectorIsDisconnected?.Invoke(connectorID);
+    public void ConnectToComponent(IConnector connectorPoint, ConnectorInfo info) {
+        detectedTarget = connectorPoint?.GetTargetObjByIndex(info.linkedTargetID)?.gameObject.GetComponent<Target>();
+        if (detectedTarget == null) { return; }
+        this.LinkToConnector(detectedTarget.ownerConnector, info);
     }
-
-    // linke connector c to other connector which is record(gameobject) by c itself.
-    static void LinkObject(Connector c)
-    {
-        if(c == null) return;
-        if(c.selectedTargetObj == null) return;
-        c.linkedTarget = c.selectedTargetObj.GetComponent<Target>();
-
-        Debug.Assert(c.linkedTarget);
-
-        if(c.linkedTarget == null) { return; }
-
-        LinkTarget(c);
+    public (IConnector, int) GetAvailableConnector() {
+        IConnector resIC = detectedTarget == null ? null : detectedTarget.ownerConnector;
+        int resTid = detectedTarget == null ? -1 : detectedTarget.targetID;
+        return (resIC, resTid);
     }
-
-    // linke connector c to other connector which is record(Target) by c itself.
-    static void LinkTarget(Connector c)
-    {
-        Debug.Assert(c.linkedTarget);
-        Debug.Assert(c.selfJoint);
-        Debug.Assert(c.linkedTarget.targetPoint);
-
-
-
-        if (c.linkedTarget == null) { return; }
-
-        if(!c.linkedTarget.LinkToTarget(c)) { return; }
-        c.linkedTarget.ownerConnector.linkedHandler.AddListener(c.SwitchLinkingSelect);
-        c.selfJoint.connectedBody = c.linkedTarget.ownerConnector.selfRigidbody;
-        c.selfJoint.connectedAnchor = (Vector2)c.linkedTarget.targetPoint.transform.localPosition;
-        c.selfJoint.enabled = true;
-
-        c.OnConnectConnector?.Invoke(c.linkedTarget.ownerConnector);
-        c.linkedTarget.ownerConnector.OnConnectorIsConnected?.Invoke(c);
-    }
-
-    // control the connector is selected or not.
-    void SwitchSelecting(bool b)
-    {
-        if (!combineMode) { return; }
-        if (selecting == b ) return;
-
-        selecting = b;
-        selfRigidbody.gravityScale = b ? 0 : 1;
-        selfCollider.isTrigger = b;
-        BreakTatgetLink();
-        if (b) ResetCompareVariable(); else LinkObject(this); 
-        SwitchTargetActive(!b);
-        linkedHandler.Invoke(b);
-    }
-
-    // control the connector to switch to combine-mode or not.
-    void SwitchCombineMode(bool b)
-    {
-        if(!b) SwitchSelecting(false);
-
-        combineMode = b;
-        selfRigidbody.freezeRotation = b;
-        SwitchTargetActive(b);
-    }
-
-    // interface imp.
-
-    public GameObject GetTargetObjByIndex(int targetID)
-    {
-        return targetList.Count > targetID ? targetList[targetID].gameObject : null;
-    }
-
-    // implement connect connector to oth connector by interface.
-
-    // not implement rotation yet
-    public void ConnectToComponent(IConnector connectorPoint, ConnectorInfo info)
-    {
-        selectedTargetObj = connectorPoint.GetTargetObjByIndex(info.linkedTargetID);
-        Debug.Assert(selectedTargetObj);
-        LinkObject(this);
-    }
-
-    public (IConnector, int) GetAvailableConnector()
-    {
-        throw new NotImplementedException();
+    public ConnectorInfo Dump() {
+        var res = new ConnectorInfo();
+        res.connectorID = this.connectorID;
+        res.linkedConnectorID = linkedTarget == null ? -1 : linkedTarget.ownerConnector.connectorID;
+        res.linkedTargetID = linkedTarget == null ? -1 : linkedTarget.targetID;
+        res.connectorRotation = Quaternion.Angle(this.gameObject.transform.rotation, Quaternion.identity);
+        return res;
     }
 }
