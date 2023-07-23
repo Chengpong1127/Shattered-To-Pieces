2629db8a1e7e284d4e88ba6560b4aa7b0fbc8fb4
Chengpong1127 <Chengpong1127@gmail.com>
2023-07-23 19:29:00 +0800

    add basic usable Bat GameComponent

---
+++ GameTopic/Assets/Scripts/CoreComponents/Bat.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bat : MonoBehaviour, ICoreComponent
+{
+    public Dictionary<string, Ability> AllAbilities { get; private set; } = new Dictionary<string, Ability>();
+
+    private void Start() {
+        AllAbilities.Add("Swing", new Ability("Swing", Swing));
+    }
+
+    private void Swing(){
+        Debug.Log("Swing");
+    }
+}
