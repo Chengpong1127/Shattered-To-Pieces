fca1c3467cbed9933c4f560db0d81e77c1a0ef43
fili-asphalt <styuiw9468546@gmail.com>
2023-07-30 15:40:12 +0800

    add skill affect Pool

--- GameTopic/Assets/Scripts/CoreComponents/SkillAffectBase.cs
+++ GameTopic/Assets/Scripts/CoreComponents/SkillAffectBase.cs
@@ -12,10 +12,20 @@ public enum SkillAffectType {
 [CreateAssetMenu(fileName = "SkillAffectBase", menuName = "AffectedObject/SkillAffectBase")]
 public class SkillAffectBase : ScriptableObject
 {
+    static public SkillAffectPool Pool { get; set; } = new SkillAffectPool();
+
     public AffectedObjectBase owner { get; set; }
     public AffectedObjectBase affectedObject { get; set; }
     public SkillAffectType type { get; set; } = SkillAffectType.SystemCtrl;
     public bool IsDebuff { get; set; } = false;
     public bool InterruptIble { get; set; } = true;
 
+}
+
+public class SkillAffectPool {
+    Queue<SkillAffectBase> queue {  get; set; } = new Queue<SkillAffectBase>();
+
+    SkillAffectPool() {
+        
+    }
 }
