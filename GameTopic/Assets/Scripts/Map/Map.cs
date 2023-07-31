08d66c5d77888bb34e442b1223c7fdc6af4957c1
Chengpong1127 <Chengpong1127@gmail.com>
2023-07-31 20:54:17 +0800

    add tilemap

--- GameTopic/Assets/Scripts/Map/Map.cs
+++ GameTopic/Assets/Scripts/Map/Map.cs
@@ -1,8 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Map : MonoBehaviour
 {
     public Transform[] SpawnPoints;
+    public Tilemap tilemap;
+    private void Awake() {
+
+    }
 }
