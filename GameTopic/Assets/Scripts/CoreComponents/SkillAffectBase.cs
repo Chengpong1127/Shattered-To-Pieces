7ed3b6041c2355db4910213b18b8e0c19918166a
fili-asphalt <styuiw9468546@gmail.com>
2023-07-31 16:50:21 +0800

    add skillAffect Pooling object

--- GameTopic/Assets/Scripts/CoreComponents/SkillAffectBase.cs
+++ GameTopic/Assets/Scripts/CoreComponents/SkillAffectBase.cs
@@ -14,18 +14,28 @@ public class SkillAffectBase : ScriptableObject
 {
     static public SkillAffectPool Pool { get; set; } = new SkillAffectPool();
 
-    public AffectedObjectBase owner { get; set; }
-    public AffectedObjectBase affectedObject { get; set; }
+    public AffectedObjectBase owner { get; set; } = null;
+    public AffectedObjectBase affectedObject { get; set; } = null;
     public SkillAffectType type { get; set; } = SkillAffectType.SystemCtrl;
     public bool IsDebuff { get; set; } = false;
-    public bool InterruptIble { get; set; } = true;
+    public bool Interruptible { get; set; } = true;
+    public bool execute { get;set; } = false;
 
 }
 
 public class SkillAffectPool {
     Queue<SkillAffectBase> queue {  get; set; } = new Queue<SkillAffectBase>();
-
+    int defaultPoolSize { get; set; } = 20;
     public SkillAffectPool() {
-        
+        for(int i = 0; i < defaultPoolSize; ++i) {
+            queue.Enqueue(new SkillAffectBase());
+        }
+    }
+
+    public SkillAffectBase GetElement() {
+        return queue.Count > 0 ? queue.Dequeue() : new SkillAffectBase();
+    }
+    public void StoreElement(SkillAffectBase poolObj) {
+        queue.Enqueue(poolObj);
     }
 }
