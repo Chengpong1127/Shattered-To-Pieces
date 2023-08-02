053af5933689dd7753613d9bb7f9b858864fafa8
Crazyfighting <crazyfighting910325@gmail.com>
2023-08-02 17:54:35 +0800

    update bat ability to Coroutine

--- GameTopic/Assets/Scripts/CoreComponents/Bat.cs
+++ GameTopic/Assets/Scripts/CoreComponents/Bat.cs
@@ -8,52 +8,59 @@ public class Bat : MonoBehaviour, ICoreComponent
     public Transform connectAnchor;
     private bool isRotating;
     private bool SkillTriggered = false;
-    private float targetRotation=0f;
+    private float targetRotation = 0f;
     private float oringnalRotation;
     private float rotationSpeed = 500f;
     private Rigidbody2D rb;
     private bool clockwise;
-    private void Start() {
-        rb=GetComponentInParent<Rigidbody2D>();
+
+    private void Start()
+    {
+        rb = GetComponentInParent<Rigidbody2D>();
         isRotating = false;
         AllAbilities.Add("SwingRight", new Ability("SwingRight", SwingRight, this));
         AllAbilities.Add("SwingLeft", new Ability("SwingLeft", SwingLeft, this));
     }
 
-    private void SwingRight(){
-        if (!isRotating&&!SkillTriggered)
+    private void SwingRight()
+    {
+        if (!isRotating && !SkillTriggered)
         {
             isRotating = true;
-            clockwise =false;
-            targetRotation = transform.rotation.eulerAngles.z-90f;
+            clockwise = false;
+            targetRotation = transform.rotation.eulerAngles.z - 90f;
+            if (targetRotation < 0f) targetRotation += 360f;
             oringnalRotation = transform.rotation.eulerAngles.z;
-            SkillTriggered=true;
+            SkillTriggered = true;
+            StartCoroutine(RotateCoroutine());
         }
     }
+
     private void SwingLeft()
     {
-        if (!isRotating&&!SkillTriggered)
+        if (!isRotating && !SkillTriggered)
         {
             isRotating = true;
             clockwise = true;
             targetRotation = transform.rotation.eulerAngles.z + 90f;
+            if (targetRotation > 360f) targetRotation -= 360f;
             oringnalRotation = transform.rotation.eulerAngles.z;
             SkillTriggered = true;
+            StartCoroutine(RotateCoroutine());
         }
     }
-    private void Update()
+
+    private IEnumerator RotateCoroutine()
     {
-
-        if (isRotating&&SkillTriggered)
+        while (isRotating && SkillTriggered)
         {
-
             float currentRotation = transform.rotation.eulerAngles.z;
             float target = targetRotation - currentRotation;
             if (target < -360f) target += 360f;
             if (target > 360f) target -= 360f;
             if (Mathf.Abs(target) > 2f)
             {
-                float step = (clockwise ? 1f : -1f) * Mathf.Min(Mathf.Abs(target), rotationSpeed * Time.deltaTime);
+                float step = (clockwise ? 1f : -1f) * Mathf.Min(Mathf.Abs(target), rotationSpeed * Time.fixedDeltaTime);
                 transform.RotateAround(connectAnchor.position, Vector3.forward, step);
             }
             else
@@ -61,8 +68,11 @@ public class Bat : MonoBehaviour, ICoreComponent
                 targetRotation = oringnalRotation;
                 isRotating = false;
             }
+
+            yield return new WaitForFixedUpdate();
         }
-        else if(SkillTriggered)
+
+        while (SkillTriggered)
         {
             float currentRotation = transform.rotation.eulerAngles.z;
             float target = targetRotation - currentRotation;
@@ -70,14 +80,15 @@ public class Bat : MonoBehaviour, ICoreComponent
             if (target > 360f) target -= 360f;
             if (Mathf.Abs(target) > 2f)
             {
-                float step = (clockwise ? -1f : 1f) * Mathf.Min(Mathf.Abs(target), rotationSpeed * Time.deltaTime);
+                float step = (clockwise ? -1f : 1f) * Mathf.Min(Mathf.Abs(target), rotationSpeed * Time.fixedDeltaTime);
                 transform.RotateAround(connectAnchor.position, Vector3.forward, step);
             }
             else
             {
                 SkillTriggered = false;
             }
-            
+
+            yield return new WaitForFixedUpdate();
         }
     }
 }
