c72d3598c070ae3f7b5a0b85d002263dd13c0935
Chengpong1127 <Chengpong1127@gmail.com>
2023-08-18 13:25:17 +0800

    move init ability to entity

--- GameTopic/Assets/Scripts/Entity/AbilityEntity.cs
+++ GameTopic/Assets/Scripts/Entity/AbilityEntity.cs
@@ -3,27 +3,18 @@ using AbilitySystem.Authoring;
 using UnityEngine;
 using System.Linq;
 
+/// <summary>
+/// AbilityEntity is an entity that has main abilities.
+/// </summary>
 public class AbilityEntity: Entity{
     [SerializeField]
     protected AbstractAbilityScriptableObject[] Abilities;
-    [SerializeField]
-    protected AbstractAbilityScriptableObject[] InitializationAbilities;
     private readonly Dictionary<AbstractAbilityScriptableObject, AbstractAbilitySpec> _abilityMap = new();
     protected override void Awake(){
         base.Awake();
         Abilities = Abilities.Where(x => x != null).ToArray();
         InitializationAbilities = InitializationAbilities.Where(x => x != null).ToArray();
-        ActivateInitializationAbilities();
         GrantCastableAbilities();
-    }
-    private void ActivateInitializationAbilities()
-    {
-        foreach (var initializationAbility in InitializationAbilities)
-        {
-            var spec = initializationAbility.CreateSpec(AbilitySystemCharacter);
-            AbilitySystemCharacter.GrantAbility(spec);
-            StartCoroutine(spec.TryActivateAbility());
-        }
     }
     private void GrantCastableAbilities()
     {
--- GameTopic/Assets/Scripts/Entity/Entity.cs
+++ GameTopic/Assets/Scripts/Entity/Entity.cs
@@ -1,14 +1,30 @@
 using AttributeSystem.Components;
 using AbilitySystem;
 using UnityEngine;
+using AbilitySystem.Authoring;
 
+/// <summary>
+/// Entity is the base class for all entities in the game. An entity has attributes and the init abilities to set up the init state of the entity.
+/// </summary>
 [RequireComponent(typeof(AttributeSystemComponent)), RequireComponent(typeof(AbilitySystemCharacter))]
-public abstract class Entity: BaseEntity{
+public class Entity: BaseEntity{
     public AttributeSystemComponent AttributeSystemComponent;
     public AbilitySystemCharacter AbilitySystemCharacter;
+    [SerializeField]
+    protected AbstractAbilityScriptableObject[] InitializationAbilities;
     protected virtual void Awake() {
-        AttributeSystemComponent = GetComponent<AttributeSystemComponent>();
-        AbilitySystemCharacter = GetComponent<AbilitySystemCharacter>();
+        AttributeSystemComponent ??= GetComponent<AttributeSystemComponent>();
+        AbilitySystemCharacter ??= GetComponent<AbilitySystemCharacter>();
         AbilitySystemCharacter.AttributeSystem = AttributeSystemComponent;
+        ActivateInitializationAbilities();
+    }
+    private void ActivateInitializationAbilities()
+    {
+        foreach (var initializationAbility in InitializationAbilities)
+        {
+            var spec = initializationAbility.CreateSpec(AbilitySystemCharacter);
+            AbilitySystemCharacter.GrantAbility(spec);
+            StartCoroutine(spec.TryActivateAbility());
+        }
     }
 }
