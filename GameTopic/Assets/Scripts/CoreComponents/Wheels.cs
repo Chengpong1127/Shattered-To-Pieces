983814f1a19dc17d13114730481a3ee7d2317028
Chengpong1127 <Chengpong1127@gmail.com>
2023-08-21 22:21:13 +0800

    Update Wheel

--- GameTopic/Assets/Scripts/CoreComponents/Wheels.cs
+++ GameTopic/Assets/Scripts/CoreComponents/Wheels.cs
@@ -1,52 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class Wheels : BaseCoreComponent, IAffectObjectOwner
+public class Wheel : BaseCoreComponent
 {
-
-    public WheelJoint2D wheelJoint;
-
-    private const float MoveForce = 100f;
-    public IAffectedObject affectedObject { get { return affectedObjectInstance; } }
-    public AffectedObjectBase affectedObjectInstance { get; set; }
-    WheelAffect affect { get; set; }
-
-    protected override void Awake()
-    {
-        affect = ScriptableObject.CreateInstance<WheelAffect>();
-        affectedObjectInstance = ScriptableObject.CreateInstance<AffectedObjectBase>();
-        affectedObject.collider = GetComponentInParent<Collider2D>();
-        affectedObject.rigidbody = GetComponentInParent<Rigidbody2D>();
-        affectedObject.joint = GetComponentInParent<AnchoredJoint2D>();
-        affectedObject.transform = gameObject.transform;
-        affect.owner = affectedObject;
-        affect.affectedObjectList.Add(affectedObject);
-        affect.wheelJoint = wheelJoint;
-        Debug.Assert(wheelJoint != null, "wheelJoint is null");
-        // AllAbilities = new Dictionary<string, GameComponentAbility>
-        // {
-        //     {"TurnLeft", new GameComponentAbility("TurnLeft",AbiliityStart, LeftRunning, AbiliityEnd,this)},
-        //     {"TurnRight", new GameComponentAbility("TurnRight", AbiliityStart, RightRunning, AbiliityEnd,this)},
-        // };
-    }
-    public void AbiliityStart()
-    {
-        affect.InvokeStart();
-    }
-    public void LeftRunning()
-    {
-        affect.direction = true;
-        affect.Invoke();
-    }
-    public void RightRunning()
-    {
-        affect.direction = false;
-        affect.Invoke();
-    }
-    public void AbiliityEnd()
-    {
-        affect.End();
-    }
-
+    
 }
 
