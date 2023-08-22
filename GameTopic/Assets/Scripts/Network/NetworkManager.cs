30c8400622865d5c4488b5c8228ca2b40e4602cf
Chengpong1127 <Chengpong1127@gmail.com>
2023-08-22 23:12:51 +0800

    add bat the network object

--- GameTopic/Assets/Scripts/Network/NetworkManager.cs
+++ GameTopic/Assets/Scripts/Network/NetworkManager.cs
@@ -3,6 +3,7 @@ using Fusion.Sockets;
 using System.Collections.Generic;
 using System;
 using UnityEngine.SceneManagement;
+using UnityEngine;
 
 public class NetworkManager : SingletonMonoBehavior<NetworkManager>, INetworkRunnerCallbacks
 {
@@ -33,77 +34,76 @@ public class NetworkManager : SingletonMonoBehavior<NetworkManager>, INetworkRun
 
     public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
     {
-        //throw new NotImplementedException();
+        Debug.Log("Player left: " + player);
     }
     public void OnConnectedToServer(NetworkRunner runner)
     {
-        
+        Debug.Log("Connected to server");
     }
 
     public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
     {
-        //throw new NotImplementedException();
+        Debug.Log("Connect failed: " + reason);
     }
 
     public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
     {
-        //throw new NotImplementedException();
+        Debug.Log("Connect request: " + request);
     }
 
     public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
     {
-        //throw new NotImplementedException();
+        Debug.Log("Custom auth response: " + data);
     }
 
     public void OnDisconnectedFromServer(NetworkRunner runner)
     {
-        //throw new NotImplementedException();
+        Debug.Log("Disconnected from server");
     }
 
     public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
     {
-        //throw new NotImplementedException();
+        Debug.Log("Host migration: " + hostMigrationToken);
     }
 
     public void OnInput(NetworkRunner runner, NetworkInput input)
     {
-        //throw new NotImplementedException();
     }
 
     public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
     {
-        //throw new NotImplementedException();
+        Debug.Log("Input missing: " + input);
     }
 
 
 
     public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ArraySegment<byte> data)
     {
-        //throw new NotImplementedException();
+        Debug.Log("Reliable data received: " + data);
     }
 
     public void OnSceneLoadDone(NetworkRunner runner)
     {
-        //throw new NotImplementedException();
+        Debug.Log("Scene load done");
     }
 
     public void OnSceneLoadStart(NetworkRunner runner)
     {
-        //throw new NotImplementedException();
+        Debug.Log("Scene load start");
     }
 
     public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
     {
-        //throw new NotImplementedException();
+        Debug.Log("Session list updated: " + sessionList);
     }
 
     public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
     {
-        //throw new NotImplementedException();
+        Debug.Log("Shutdown: " + shutdownReason);
     }
 
     public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
     {
-        //throw new NotImplementedException();
+        Debug.Log("User simulation message: " + message);
     }
 }
