ce18da78876287d11191ce7e9699d359a27939ba
fili-asphalt <styuiw9468546@gmail.com>
2023-08-30 14:47:21 +0800

    Let Leg control gravity.

--- GameTopic/Assets/Scripts/CoreComponents/Leg.cs
+++ GameTopic/Assets/Scripts/CoreComponents/Leg.cs
@@ -5,10 +5,32 @@ using UnityEngine;
 public class Leg : BaseCoreComponent , IBodyControlable {
     public BaseCoreComponent body { get; private set; }
 
+    HashSet<GameObject> touchObj = new HashSet<GameObject>();
+
     protected override void Awake() {
         body = this;
 
 
         base.Awake();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        var obj = collision.gameObject.GetComponent<BaseCoreComponent>();
+        if (obj == null) {
+            touchObj.Add(collision.gameObject);
+            body.Root.BodyRigidbody.gravityScale = 0;
+        } else {
+            if (!obj.HasTheSameRootWith(body)) {
+                touchObj.Add(collision.gameObject);
+                body.Root.BodyRigidbody.gravityScale = 0;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        touchObj.Remove(collision.gameObject);
+        if (touchObj.Count == 0) {
+            body.Root.BodyRigidbody.gravityScale = 1;
+        }
     }
 }
