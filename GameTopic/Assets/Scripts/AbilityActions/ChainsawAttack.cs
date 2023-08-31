8ee3a6c49f315a4378ea33c8228f5baf5f37f68e
fili-asphalt <styuiw9468546@gmail.com>
2023-08-31 18:49:03 +0800

    add chainsaw attack animation and damage effect

---
+++ GameTopic/Assets/Scripts/AbilityActions/ChainsawAttack.cs
@@ -0,0 +1,52 @@
+using AbilitySystem;
+using AbilitySystem.Authoring;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ChainsawAttack", menuName = "Ability/ChainsawAttack")]
+public class ChainsawAttack : AbstractAbilityScriptableObject {
+    public GameplayEffectScriptableObject BatDamageEffect;
+    public override AbstractAbilitySpec CreateSpec(AbilitySystemCharacter owner) {
+        var spec = new ChainsawAttackSpec(this, owner) {
+            DamageEffect = BatDamageEffect
+        };
+        return spec;
+    }
+
+    public class ChainsawAttackSpec : EntityAbilitySpec {
+        public GameplayEffectScriptableObject DamageEffect;
+
+        Animator entityAnimator;
+        IEntityTriggerable entityTriggerable;
+        public ChainsawAttackSpec(AbstractAbilityScriptableObject ability, AbilitySystemCharacter owner) : base(ability, owner) {
+            entityAnimator = (SelfEntity as BaseCoreComponent)?.BodyAnimator ?? throw new System.ArgumentNullException("The entity should have animator.");
+            entityTriggerable = (SelfEntity as IEntityTriggerable) ?? throw new System.ArgumentNullException("The entity should have entity triggerable.");
+        }
+
+        public override void CancelAbility() {
+            return;
+        }
+
+        public override bool CheckGameplayTags() {
+            return true;
+        }
+
+        protected override IEnumerator ActivateAbility() {
+            entityAnimator.SetTrigger("ATKTrigger");
+            entityTriggerable.OnTriggerEntity += TriggerAction;
+            yield return new WaitUntil(() => entityAnimator.GetCurrentAnimatorStateInfo(0).IsName("Attack"));
+            yield return new WaitUntil(() => !entityAnimator.GetCurrentAnimatorStateInfo(0).IsName("Attack"));
+            entityTriggerable.OnTriggerEntity -= TriggerAction;
+        }
+
+        protected override IEnumerator PreActivate() {
+            yield return null;
+        }
+
+        private void TriggerAction(Entity other) {
+            this.TriggerEvent(EventName.GameEffectManagerEvents.RequestGiveGameEffect, SelfEntity, other, DamageEffect);
+            entityTriggerable.OnTriggerEntity -= TriggerAction;
+        }
+    }
+}
--- GameTopic/Assets/Scripts/CoreComponents/Chainsaw.cs
+++ GameTopic/Assets/Scripts/CoreComponents/Chainsaw.cs
@@ -1,6 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chainsaw : BaseCoreComponent{
+public class Chainsaw : BaseCoreComponent, IEntityTriggerable {
+    public event Action<Entity> OnTriggerEntity;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        var entity = other.GetComponent<Entity>();
+        if (entity != null) {
+            if (entity is BaseCoreComponent coreComponent && HasTheSameRootWith(coreComponent)) return;
+            OnTriggerEntity?.Invoke(entity);
+        }
+    }
+
+    protected override void Awake() {
+        base.Awake();
+        AttackDecorator.Instance.Decorate(this);
+    }
 }
