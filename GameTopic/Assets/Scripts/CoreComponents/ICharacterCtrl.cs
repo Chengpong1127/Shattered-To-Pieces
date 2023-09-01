71a21b09da4a4868c0e8b039dbc257dd1cf8681e
fili-asphalt <styuiw9468546@gmail.com>
2023-09-01 22:01:49 +0800

    packaging rigidbody for player move

--- GameTopic/Assets/Scripts/CoreComponents/ICharacterCtrl.cs
+++ GameTopic/Assets/Scripts/CoreComponents/ICharacterCtrl.cs
@@ -3,5 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public interface ICharacterCtrl {
-    public CharacterController Controller { get; }
+    public bool Landing { get; }
+    public void Move(Vector3 Motion, ForceMode2D Mode);
+    public void Push(Vector3 Motion);
+    public void Bondage();
 }
--- GameTopic/Assets/Scripts/CoreComponents/Leg.cs
+++ GameTopic/Assets/Scripts/CoreComponents/Leg.cs
@@ -14,23 +14,23 @@ public class Leg : BaseCoreComponent , IBodyControlable {
         base.Awake();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision) {
-        var obj = collision.gameObject.GetComponent<BaseCoreComponent>();
-        if (obj == null) {
-            touchObj.Add(collision.gameObject);
-            body.Root.BodyRigidbody.gravityScale = 0;
-        } else {
-            if (!obj.HasTheSameRootWith(body)) {
-                touchObj.Add(collision.gameObject);
-                body.Root.BodyRigidbody.gravityScale = 0;
-            }
-        }
-    }
+    // private void OnTriggerEnter2D(Collider2D collision) {
+    //     var obj = collision.gameObject.GetComponent<BaseCoreComponent>();
+    //     if (obj == null) {
+    //         touchObj.Add(collision.gameObject);
+    //         body.Root.BodyRigidbody.gravityScale = 0;
+    //     } else {
+    //         if (!obj.HasTheSameRootWith(body)) {
+    //             touchObj.Add(collision.gameObject);
+    //             body.Root.BodyRigidbody.gravityScale = 0;
+    //         }
+    //     }
+    // }
 
-    private void OnTriggerExit2D(Collider2D collision) {
-        touchObj.Remove(collision.gameObject);
-        if (touchObj.Count == 0) {
-            body.Root.BodyRigidbody.gravityScale = 1;
-        }
-    }
+    // private void OnTriggerExit2D(Collider2D collision) {
+    //     touchObj.Remove(collision.gameObject);
+    //     if (touchObj.Count == 0) {
+    //         body.Root.BodyRigidbody.gravityScale = 1;
+    //     }
+    // }
 }
