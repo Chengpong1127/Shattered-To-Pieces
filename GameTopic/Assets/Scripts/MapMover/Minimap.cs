14a2a1ce37653e671cf8d7223e805676e2610456
Crazyfighting <crazyfighting910325@gmail.com>
2023-09-01 20:34:52 +0800

    update map2

---
+++ GameTopic/Assets/Scripts/MapMover/Minimap.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    [SerializeField]
+    public Transform player;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        this.transform.position =new Vector3( player.position.x,player.position.y,this.transform.position.z);
+    }
+}
