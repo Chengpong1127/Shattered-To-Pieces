30912aca7b8eb0990f969d8e3bb59ca4b09f8020
Asphalt <Styuiw9468546@gmail.com>
2023-09-23 01:43:02 +0800

    add InGameUI control script

--- GameTopic/Assets/UI/Scripts/NewVersionUI/InGameUI.cs
+++ GameTopic/Assets/UI/Scripts/NewVersionUI/InGameUI.cs
@@ -1,14 +1,26 @@
 using Cysharp.Threading.Tasks;
+using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
-using static UnityEngine.UI.Image;
 
-public class AssemblyUI : NetworkBehaviour {
+public class InGameUI : NetworkBehaviour {
     IAssemblyRoom room;
-
     [SerializeField] SkillBinder Binder;
 
+
+    private BasePlayer player;
+    private AbilityManager abilityManager;
+
+    private async void Awake() {
+        player = GetComponent<BasePlayer>();
+        await UniTask.WaitUntil(() => player.IsAlive.Value == true);
+        if (IsServer) {
+            abilityManager = player.SelfDevice.AbilityManager;
+            GameEvents.AbilityManagerEvents.OnSetAbilityToEntry += (_, _) => { };// put function at here
+            GameEvents.AbilityManagerEvents.OnSetAbilityOutOfEntry += _ => { };// put function at here
+        }
+    }
 
     private async void Start() {
         GameObject impRoom = GameObject.Find("RoomManager");
@@ -22,11 +34,11 @@ public class AssemblyUI : NetworkBehaviour {
         RefreshAllSkillBox();
     }
 
+
     void BindAbilityToEntry(int origin, int newID, GameComponentAbility ability) {
 
-        if(newID == -1) { room.AbilityManager.SetAbilityOutOfEntry(ability); }
-        else { room.AbilityManager.SetAbilityToEntry(newID, ability); }
-        
+        if (newID == -1) { room.AbilityManager.SetAbilityOutOfEntry(ability); } else { room.AbilityManager.SetAbilityToEntry(newID, ability); }
+
         Binder.SetDisply(origin, origin != -1 ? room.AbilityManager.AbilityInputEntries[origin].Abilities : room.AbilityManager.GetAbilitiesOutOfEntry());
         Binder.SetDisply(newID, newID != -1 ? room.AbilityManager.AbilityInputEntries[newID].Abilities : room.AbilityManager.GetAbilitiesOutOfEntry());
     }
