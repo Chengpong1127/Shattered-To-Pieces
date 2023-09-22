d3b1d9b2b2b3366d7ff136f04b1d24dda4dfee48
Asphalt <Styuiw9468546@gmail.com>
2023-09-22 18:17:32 +0800

    add imgs and scripts file

---
+++ GameTopic/Assets/UI/Scripts/AssemblyUI.cs
@@ -0,0 +1,21 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class AssemblyUI : NetworkBehaviour {
+    [SerializeField] GameObject Pendant;
+    [SerializeField] GameObject Skills;
+    [SerializeField] GameObject CostDisplay;
+    [SerializeField] GameObject SideBar;
+    [SerializeField] GameObject ExitConfirm;
+
+
+
+
+
+
+
+
+
+
+
+}
