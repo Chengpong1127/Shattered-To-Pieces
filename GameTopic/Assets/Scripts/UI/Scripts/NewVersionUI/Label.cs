d1927746e634a10cfe8a71863dbaade5f75e8e7f
Asphalt <Styuiw9468546@gmail.com>
2023-10-05 20:10:14 +0800

    add label and sideBar control scripts.

--- GameTopic/Assets/Scripts/UI/Scripts/NewVersionUI/Label.cs
+++ GameTopic/Assets/Scripts/UI/Scripts/NewVersionUI/Label.cs
@@ -1,7 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Label : MonoBehaviour
-{
+public class Label : MonoBehaviour, IPointerClickHandler {
+    [SerializeField] public LabelColor labelColor;
+    public SideBar sideBar { get; set; }
+    public int LabelID { get; set; }
+
+    public void OnPointerClick(PointerEventData eventData) {
+        Debug.Log("Click label : " + gameObject.name);
+
+        // call sideBar click label function.
+        sideBar?.OnClickLabel(LabelID);
+    }
+}
+
+
+[System.Serializable]
+public struct LabelColor {
+    public Color Dark { get;set; }
+    public Color Light { get; set; }
 }
--- GameTopic/Assets/Scripts/UI/Scripts/NewVersionUI/SideBar.cs
+++ GameTopic/Assets/Scripts/UI/Scripts/NewVersionUI/SideBar.cs
@@ -4,4 +4,21 @@ using UnityEngine;
 
 public class SideBar : MonoBehaviour
 {
+    [SerializeField] List<Label> labels;
+
+    private void Awake() {
+
+        // setting label variables.
+        int labelID = 0;
+        labels.ForEach(label => {
+            label.sideBar = this;
+            label.LabelID = labelID++;
+        });
+    }
+
+
+
+    public void OnClickLabel(int id) {
+
+    }
 }
