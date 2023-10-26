b185af2c6a4b51b47ea3095582e3e47e8c86fad1
Chengpong1127 <Chengpong1127@gmail.com>
2023-10-27 00:45:22 +0800

    try get player ok

--- GameTopic/Assets/Scripts/AI/BaseAIAgent.cs
+++ GameTopic/Assets/Scripts/AI/BaseAIAgent.cs
@@ -1,16 +1,52 @@
 using UnityEngine;
 using NPBehave;
+using Cysharp.Threading.Tasks;
+using System.Linq;
 
 
 
 public abstract class BaseAIAgent: Entity{
     public Root BehaviorTree;
     public abstract Root GetBehaviorTree();
-    protected override void Start() {
+    protected override async void Start() {
         base.Start();
+        await UniTask.WaitUntil(() => IsServer || IsClient);
         if (IsServer){
             BehaviorTree = GetBehaviorTree();
             BehaviorTree.Start();
         }
     }
+    public override void Die()
+    {
+        if (BehaviorTree != null && BehaviorTree.CurrentState == Node.State.ACTIVE) BehaviorTree.Stop();
+        base.Die();
+    }
+
+    protected virtual void UpdateBlackboard()
+    {
+        var attributeMap = AttributeSystemComponent.GetFullAttributeDictionary();
+        attributeMap.ToList().ForEach(pair => BehaviorTree.Blackboard[pair.Key.Name] = pair.Value.CurrentValue);
+    }
+
+    protected BaseEntity GetClosestEntity(float radius, params string[] tags){
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, radius);
+        BaseEntity[] entities = colliders
+            .Select(collider => collider.GetComponentInParent<BaseEntity>())
+            .Where(entity => entity != null)
+            .Where(entity => tags.All(tag => entity.Taggable.HasTag(tag))).ToArray();
+        if (entities.Length == 0) return null;
+        BaseEntity closestEntity = entities[0];
+        float closestDistance = Vector2.Distance(transform.position, closestEntity.transform.position);
+        for (int i = 1; i < entities.Length; i++)
+        {
+            float distance = Vector2.Distance(transform.position, entities[i].transform.position);
+            if (distance < closestDistance){
+                closestDistance = distance;
+                closestEntity = entities[i];
+            }
+        }
+        return closestEntity;
+    }
+
+    
 }
--- GameTopic/Assets/Scripts/AI/BasicAI.cs
+++ GameTopic/Assets/Scripts/AI/BasicAI.cs
@@ -2,17 +2,44 @@
 
 using UnityEngine;
 using NPBehave;
+using Cysharp.Threading.Tasks;
+using System.Linq;
 
 public class BasicAI : BaseAIAgent
 {
     public override Root GetBehaviorTree()
     {
         return new Root(
-            new Action(() => {
-                Debug.Log("Hello World!");
-                return true;
-            })
+            new Service(0.5f, UpdateBlackboard,
+                new Selector(
+                    new BlackboardCondition("TargetPlayer", Operator.IS_SET, Stops.IMMEDIATE_RESTART,
+                        new Action(() => {
+                            if(BehaviorTree.Blackboard["TargetPlayer"] == null){
+                                Debug.Log("TargetPlayer is null");
+                            }
+                            else{
+                                Debug.Log("TargetPlayer is not null");
+                            }
+                        }
+                        )
+                            
+                    ),
+                    new Action(() => Debug.Log("TargetPlayer is not set"))
+                )
+            )
         );
+    }
+
+    protected override void UpdateBlackboard()
+    {
+        BehaviorTree.Blackboard["TargetPlayer"] = GetClosestEntity(10, "Player");
+        base.UpdateBlackboard();
+    }
+    public void Jump(){
+        BodyRigidbody.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+    }
+    public void Move(float direction){
+        BodyRigidbody.velocity = new Vector2(direction * 5, BodyRigidbody.velocity.y);
     }
 
 }
