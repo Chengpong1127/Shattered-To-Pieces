2e7bcd8052de293a3008653f8590b378595698ba
Chengpong1127 <Chengpong1127@gmail.com>
2023-10-27 17:40:24 +0800

    add basicAI prefab

--- GameTopic/Assets/Scripts/AI/BasicAI.cs
+++ GameTopic/Assets/Scripts/AI/BasicAI.cs
@@ -10,6 +10,7 @@ public class BasicAI : BaseAIAgent
 {
     public float JumpForce = 10;
     public float MoveSpeed = 0.3f;
+    public float ChaseRange = 10;
     public float AttackRange = 3;
     public float AttackCooldown = 1;
     public GameplayEffectScriptableObject AttackEffect;
@@ -17,20 +18,19 @@ public class BasicAI : BaseAIAgent
     {
         return new Root(
             new Service(1f, UpdateBlackboard,
-                new BlackboardCondition("TargetPlayer", Operator.IS_NOT_EQUAL, "null", Stops.IMMEDIATE_RESTART,
-                    new Selector(
-                        new BlackboardCondition("TargetPlayerDistance", Operator.IS_GREATER, AttackRange, Stops.IMMEDIATE_RESTART,
-                            new Sequence(
-                                new Action(ChasePlayer)
-                            )
-                        ),
-                        new BlackboardCondition("TargetPlayerDistance", Operator.IS_SMALLER_OR_EQUAL, AttackRange, Stops.IMMEDIATE_RESTART,
-                            new Sequence(
-                                new Action(Attack),
-                                new Wait(AttackCooldown)
-                            )
+                new Selector(
+                    new BlackboardCondition("AttackRangePlayer", Operator.IS_NOT_EQUAL, "null", Stops.IMMEDIATE_RESTART,
+                        new Sequence(
+                            new Action(Attack),
+                            new Wait(AttackCooldown)
                         )
-                    )
+                    ),
+                    new BlackboardCondition("TargetPlayer", Operator.IS_NOT_EQUAL, "null", Stops.IMMEDIATE_RESTART,
+                        new Sequence(
+                            new Action(ChasePlayer)
+                        )
+                    ),
+                    new Action(() => Move(0))
                 )
             )
         );
@@ -38,30 +38,27 @@ public class BasicAI : BaseAIAgent
 
     protected override void UpdateBlackboard()
     {
-        var TargetPlayer = GetClosestEntity(10, "GameComponent");
+        var TargetPlayer = GetClosestEntity(ChaseRange, "GameComponent");
+        var AttackRangePlayer = GetClosestEntity(AttackRange, "GameComponent");
         if (TargetPlayer != null) BehaviorTree.Blackboard["TargetPlayer"] = TargetPlayer;
         else BehaviorTree.Blackboard["TargetPlayer"] = "null";
-        BehaviorTree.Blackboard["TargetPlayerDistance"] = Vector2.Distance(BodyTransform.position, (BehaviorTree.Blackboard["TargetPlayer"] as Entity)?.BodyTransform.position ?? Vector2.zero);
-        BehaviorTree.Blackboard["TargetPlayerPosition"] = (BehaviorTree.Blackboard["TargetPlayer"] as Entity)?.BodyTransform.position ?? Vector2.zero;
+        if (AttackRangePlayer != null) BehaviorTree.Blackboard["AttackRangePlayer"] = AttackRangePlayer;
+        else BehaviorTree.Blackboard["AttackRangePlayer"] = "null";
         base.UpdateBlackboard();
     }
     protected Action.Result ChasePlayer(bool shouldCancel = false){
         if (shouldCancel) return Action.Result.FAILED;
-        if ((float)BehaviorTree.Blackboard["TargetPlayerDistance"] <= AttackRange) return Action.Result.SUCCESS;
         var targetPlayer = BehaviorTree.Blackboard["TargetPlayer"] as Entity;
-        Move(targetPlayer.transform.position.x > transform.position.x ? 1 : -1);
+        if (BehaviorTree.Blackboard["AttackRangePlayer"].ToString() != "null") return Action.Result.SUCCESS;
+        Move(targetPlayer.BodyTransform.position.x > BodyTransform.position.x ? 1 : -1);
         return Action.Result.PROGRESS;
-    }
-
-    public void Jump(){
-        BodyRigidbody.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
     }
     public void Move(float direction){
         BodyRigidbody.velocity = new Vector2(direction * MoveSpeed, BodyRigidbody.velocity.y);
     }
     public Action.Result Attack(bool shouldCancel = false){
         if (shouldCancel) return Action.Result.FAILED;
-        var targetPlayer = BehaviorTree.Blackboard["TargetPlayer"] as Entity;
+        var targetPlayer = BehaviorTree.Blackboard["AttackRangePlayer"] as Entity;
         if (targetPlayer == null) return Action.Result.FAILED;
         GameEvents.GameEffectManagerEvents.RequestGiveGameEffect.Invoke(this, targetPlayer, AttackEffect);
         return Action.Result.SUCCESS;
