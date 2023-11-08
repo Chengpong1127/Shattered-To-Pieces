79e2fbfd6d514c8f645c7f11caf333dbe6bb693d
Chengpong1127 <Chengpong1127@gmail.com>
2023-11-08 15:51:45 +0800

    update exit panel

--- GameTopic/Assets/Scripts/UI/Panel/GamePanel.cs
+++ GameTopic/Assets/Scripts/UI/Panel/GamePanel.cs
@@ -3,16 +3,22 @@ using DG.Tweening;
 using Cysharp.Threading.Tasks;
 using MonsterLove.StateMachine;
 using System;
+using UnityEngine.UI;
 
 public class GamePanel : MonoBehaviour
 {
     public event Action<string> OnSendMessage;
     public StateMachine<GamePanelState> StateMachine;
+    public CanvasGroup Background;
+    public Transform PanelTransform;
     void Awake()
     {
         StateMachine = StateMachine<GamePanelState>.Initialize(this);
         StateMachine.ChangeState(GamePanelState.Hiding);
-        transform.localScale = Vector3.zero;
+        PanelTransform.localScale = Vector3.zero;
+        Background.alpha = 0;
+        Background.interactable = false;
+        Background.blocksRaycasts = false;
     }
 
     public async void EnterScene(){
@@ -24,10 +30,20 @@ public class GamePanel : MonoBehaviour
         StateMachine.ChangeState(GamePanelState.Hiding);
     }
     protected virtual async UniTask EnterSceneAnimation(){
-        await transform.DOScale(1, 0.5f).SetEase(Ease.OutBack).ToUniTask();
+        await UniTask.WhenAll(
+            DOTween.To(() => Background.alpha, x => Background.alpha = x, 1, 0.2f).ToUniTask(),
+            PanelTransform.DOScale(1, 0.5f).SetEase(Ease.OutBack).ToUniTask()
+        );
+        Background.interactable = true;
+        Background.blocksRaycasts = true;
     }
     protected virtual async UniTask ExitSceneAnimation(){
-        await transform.DOScale(0, 0.5f).SetEase(Ease.InBack).ToUniTask();
+        await UniTask.WhenAll(
+            DOTween.To(() => Background.alpha, x => Background.alpha = x, 0, 0.2f).ToUniTask(),
+            PanelTransform.DOScale(0, 0.5f).SetEase(Ease.InBack).ToUniTask()
+        );
+        Background.interactable = false;
+        Background.blocksRaycasts = false;
     }
 
     public void PanelSendMessage(string message){
