c5dd03b421d92e68565d9ec5bb56d7ee66be117e
Asphalt <Styuiw9468546@gmail.com>
2023-11-17 02:34:05 +0800

    add control room using ability judge.

--- GameTopic/Assets/Scripts/CoreComponents/ControlRoom.cs
+++ GameTopic/Assets/Scripts/CoreComponents/ControlRoom.cs
@@ -9,4 +9,27 @@ using GameplayTagNamespace.Authoring;
 
 public class ControlRoom : BaseCoreComponent, IDeviceRoot {
     public Device Device { get; set; }
+
+    int currentRunningAbility = 0;
+    override protected void Awake() {
+        base.Awake();
+
+        GameEvents.AbilityRunnerEvents.OnLocalInputStartAbility += StartAbilityListener;
+        GameEvents.AbilityRunnerEvents.OnLocalInputCancelAbility += CancelAbilityListener;
+    }
+    override public void OnDestroy() {
+        GameEvents.AbilityRunnerEvents.OnLocalInputStartAbility -= StartAbilityListener;
+        GameEvents.AbilityRunnerEvents.OnLocalInputCancelAbility -= CancelAbilityListener;
+
+        base.OnDestroy();
+    }
+
+    void StartAbilityListener(int skillID) {
+        if(currentRunningAbility == 0) { BodyAnimator.SetTrigger("OnUseSkill"); }
+        currentRunningAbility++;
+    }
+    void CancelAbilityListener(int skillID) {
+        currentRunningAbility--;
+        if (currentRunningAbility == 0) { BodyAnimator.SetTrigger("Idle"); }
+    }
 }
