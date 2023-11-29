21848b7265a7dc6a516efdc1614fb0de53800321
Chengpong1127 <Chengpong1127@gmail.com>
2023-11-30 02:47:32 +0800

    Update sorting order and colors in UI elements

--- GameTopic/Assets/Scripts/UI/Scripts/PriceCtrl.cs
+++ GameTopic/Assets/Scripts/UI/Scripts/PriceCtrl.cs
@@ -6,10 +6,20 @@ using UnityEngine;
 public class PriceCtrl : MonoBehaviour
 {
     [SerializeField] TMP_Text selfTMP;
+    [SerializeField] Color normalColor;
+    [SerializeField] Color notEnoughColor;
+    void Awake()
+    {
+        Debug.Assert(selfTMP != null);
+    }
 
     public void SetPrice(int p) {
-        if(selfTMP == null || p < 0) { return; }
-
         selfTMP.text = p.ToString();
+    }
+    public void SetNormalColor() {
+        selfTMP.color = normalColor;
+    }
+    public void SetNotEnoughColor() {
+        selfTMP.color = notEnoughColor;
     }
 }
