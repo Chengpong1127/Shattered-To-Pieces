8349a228096b0a8a1079ab082d51f96188b02a7d
Crazyfighting <crazyfighting910325@gmail.com>
2023-11-30 03:42:21 +0800

    adjust map description button

--- GameTopic/Assets/Scripts/UI/Map/MapDetailController.cs
+++ GameTopic/Assets/Scripts/UI/Map/MapDetailController.cs
@@ -2,23 +2,23 @@ using UnityEngine;
 using UnityEngine.UI;
 public class MapDetailController : MonoBehaviour
 {
-    [SerializeField]
-    private Text MapNameText;
+    //[SerializeField]
+    //private Text MapNameText;
     [SerializeField]
     private Text MapDescriptionText;
-    [SerializeField]
-    private Image MapImage;
+    //[serializefield]
+    //private image mapimage;
     void Awake()
     {
-        Debug.Assert(MapNameText != null);
+        //Debug.Assert(MapNameText != null);
         Debug.Assert(MapDescriptionText != null);
-        Debug.Assert(MapImage != null);
+        //Debug.Assert(MapImage != null);
     
     }
 
     public void SetMapInfo(MapInfo mapInfo){
-        MapNameText.text = mapInfo.MapName;
+        //MapNameText.text = mapInfo.MapName;
         MapDescriptionText.text = mapInfo.MapDescription;
-        MapImage.sprite = mapInfo.MapImage;
+        //MapImage.sprite = mapInfo.MapImage;
     }
 }
