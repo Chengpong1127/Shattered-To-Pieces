beaab7bd31ab2348443dd8c61aa24bb852db6583
Crazyfighting <crazyfighting910325@gmail.com>
2023-12-07 03:35:56 +0800

    update map record

--- GameTopic/Assets/Scripts/UI/PlayerProfile/SingleMapRecordController.cs
+++ GameTopic/Assets/Scripts/UI/PlayerProfile/SingleMapRecordController.cs
@@ -5,10 +5,32 @@ public class SingleMapRecordController : MonoBehaviour
 {
     [SerializeField] private Text MapNameText;
     [SerializeField] private Text RecordText;
-
+    [SerializeField] private Sprite[] Medal;
+    [SerializeField] private Image Background;
+    [SerializeField] private GameObject particle;
     public void SetRecord(string mapName, int record)
     {
         MapNameText.text = mapName;
         RecordText.text = record.ToString();
+        switch (record){
+            case 0:
+                particle.SetActive(false);
+                break;
+            case 1:
+            case 2:
+                particle.SetActive(false);
+                Background.sprite = Medal[0];
+                break;
+            case 3:
+            case 4:
+                particle.SetActive(false);
+                Background.sprite = Medal[1];
+                break;
+            default:
+                particle.SetActive(true);
+                particle.GetComponent<ParticleSystem>().Play();
+                Background.sprite = Medal[2];
+                break;
+        }
     }
 }
